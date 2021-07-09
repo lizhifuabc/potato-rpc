78b166b68a4b04462f342a7652b79a1d5e6eadf4
lizhifu <lizhifuabc@163.com>
2021-07-09 10:40:15 +0800

    IP

--- rpc-spring-boot-starter-autoconfigure/src/main/java/com/potato/rpc/util/IPUtil.java
+++ rpc-spring-boot-starter-autoconfigure/src/main/java/com/potato/rpc/util/IPUtil.java
@@ -1,18 +1,8 @@
 package com.potato.rpc.util;
 
-import org.springframework.util.ObjectUtils;
 
 import java.net.*;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Set;
 
-/**
- * Copyright (C) 2018
- * All rights reserved
- * User: yulong.zhang
- * Date:2018年11月23日11:13:33
- */
 public class IPUtil {
     public static String getIp(){
         String ip = null;
